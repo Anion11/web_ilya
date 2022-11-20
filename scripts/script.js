7eae7e81b2b2821534da871d2c534530d3a90e51
Wolahi <103067992+Wolahi@users.noreply.github.com>
2022-11-20 21:20:08 +0300

    change img

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,40 @@
+var total_pics_num = 2; // колличество изображений
+var interval = 5000;    // задержка между изображениями
+var time_out = 1;       // задержка смены изображений
+var i = 0;
+var timeout;
+var opacity = 100;
+
 window.onload = function(){
 	document.querySelector("#imgZipCode").addEventListener("keypress", function (evt) {
     if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
     {
         evt.preventDefault();
     }
-
 });
 }
-let image = document.querySelector('.containerImage');
-let frame = image.querySelectorAll('img');
-let timeout = null;
 
-containerImage.addEventListener('mouseenter', function() {
-  let time = 3; // Время перелистывания
-  let i = 0;
-
-  change();
-
-  function change() {
-    frame[i].style.zIndex = 100;
-    if (frame[++i]) {
-      // Если существует следующая картинка - функция вызывает сама себя.
-      timeout = setTimeout(change, time); // Уже с увеличенным временем.
-    }
+function fade_to_next() {
+  opacity--;
+  var k = i + 1;
+  var image_now = 'img_' + i;
+  if (i == total_pics_num) k = 1;
+  var image_next = 'img_' + k;
+  document.getElementById(image_now).style.opacity = opacity/100;
+  document.getElementById(image_now).style.filter = 'alpha(opacity='+ opacity +')';
+  document.getElementById(image_next).style.opacity = (100-opacity)/100;
+  document.getElementById(image_next).style.filter = 'alpha(opacity='+ (100-opacity) +')';
+  timeout = setTimeout("fade_to_next()",time_out);
+  if (opacity==1) {
+    opacity = 100;
+    clearTimeout(timeout);
   }
-});
+}
 
-containerImage.addEventListener('mouseleave', function() {
-  clearTimeout(timeout);
-  for( let i = 0; i < frame.length; i++ ){
-    frame[i].style.zIndex = 0;
-  }
-});
+setInterval (
+  function() {
+    i++;
+    if (i > total_pics_num) i=1;
+    fade_to_next();
+  }, interval
+);
