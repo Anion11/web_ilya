a91a226bf9453a65710c3679d9afbc5e3b13ca8b
Anion11 <nik.klyukoit@bk.ru>
2022-11-20 21:21:06 +0300

    qqq

--- scripts/script.js
+++ scripts/script.js
@@ -7,28 +7,3 @@ window.onload = function(){
 
 });
 }
-let image = document.querySelector('.containerImage');
-let frame = image.querySelectorAll('img');
-let timeout = null;
-
-containerImage.addEventListener('mouseenter', function() {
-  let time = 3; // Время перелистывания
-  let i = 0;
-
-  change();
-
-  function change() {
-    frame[i].style.zIndex = 100;
-    if (frame[++i]) {
-      // Если существует следующая картинка - функция вызывает сама себя.
-      timeout = setTimeout(change, time); // Уже с увеличенным временем.
-    }
-  }
-});
-
-containerImage.addEventListener('mouseleave', function() {
-  clearTimeout(timeout);
-  for( let i = 0; i < frame.length; i++ ){
-    frame[i].style.zIndex = 0;
-  }
-});
