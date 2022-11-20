f984b3c5da2b7299efd9427c65869cdcd48f0369
Anion11 <nik.klyukoit@bk.ru>
2022-11-20 20:38:43 +0300

    firstmain

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
 window.onload = function(){
-	
+	document.querySelector("#imgZipCode").addEventListener("keypress", function (evt) {
+    if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
+    {
+        evt.preventDefault();
+    }
+});
 }
