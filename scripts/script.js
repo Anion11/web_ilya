0c1c8c1346b4a0febac4e4ef8d8ab17545b2756f
Anion11 <nik.klyukoit@bk.ru>
2022-12-05 15:12:38 +0300

    adapt window size

--- scripts/script.js
+++ scripts/script.js
@@ -8,16 +8,19 @@ var opacity = 100;
 var isPause;
 var lastScrollTop = 50; //После какого значения появлется хэдер
 var links;
-const vh = window.innerHeight * 0.01;
+if (sessionStorage.length == 0){
+    var firstHeightWindow = window.innerHeight * 0.01;
+    sessionStorage.setItem("myKey",JSON.stringify(firstHeightWindow));
+}
+var vh = JSON.parse(sessionStorage.getItem("myKey"));
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 window.onload = function(){
 	document.querySelector("#imgZipCode").addEventListener("keypress", function (evt) {
     if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
     {
         evt.preventDefault();
     }
- 
+    
 });
    links = document.getElementsByClassName("disActiveLink");
    activeLink(0,6,12);
@@ -70,7 +73,6 @@ window.addEventListener('scroll', function() {
     var st = window.pageYOffset;
     if (st > lastScrollTop){
         spawnHeader();
-        console.log(st);
    } else {
         hideHeader();
    }
